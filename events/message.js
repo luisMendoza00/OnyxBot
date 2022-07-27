928c06ff243ce4de14a42d3b5aff4a98a8ceacf5
luisMendoza00 <71712459+luisMendoza00@users.noreply.github.com>
2022-07-26 21:12:46 -0500

    fixes

--- events/message.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'message',
-    execute(message) {
-        message.reply('Hello World!')
-        .then(() => console.log('Sent message'))
-        .catch(console.error);
-    },
-};
--- functions/searchCards.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'searchCards',
-    execute() {
-        console.log('searchCards');
-    },
-};
-
-module.id = 'searchCards';
